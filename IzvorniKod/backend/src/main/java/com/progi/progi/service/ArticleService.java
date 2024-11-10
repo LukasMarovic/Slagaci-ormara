6d53fcaf67c5a7749ecc5d3e7f898dbe67bb2d3e
bajan-bato <toni.bajan@fer.hr>
2024-11-10 23:05:51 +0100

    Učitavanje featured artikala
    U klasi ArticleController je updateana funkcija getArticles, a u klasi ArticleService j dodano getFeatured
    getFeatured vraća 8 nasumičnih artikala iz baze podataka u json formatu koji sadrži id i naziv, svih 8 su u listi
    Htio sam staviti da se i slike dodaju u bazu, ali onda je sve prestalo raditi pa koristite placeholdere i dalje
    Trenutno je taj mapping stavljen na @GetMapping("/"), to promijenite na "/getFeatures" ili nešto i to će onda biti adresa na koju šaljete get zahtjeve
    imate schema.sql što mi je privremena baza za testiranje ako ju hoćete testirati, ako promjenite nešto ili odkomentirate onda odite i u Article objekt i odkomentirajte gettere i settere i varijable koje ste odkomentirali u bazi
    Također provjerite kako se zovu varijable koje su zapisane u JSON datoteci, trebale bi se zvati idArtikla i nazivArtikla, to možete promijeniti u Article klasi ako hoćete

--- IzvorniKod/backend/src/main/java/com/progi/progi/service/ArticleService.java
+++ IzvorniKod/backend/src/main/java/com/progi/progi/service/ArticleService.java
@@ -5,6 +5,10 @@ import com.progi.progi.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Service
 public class ArticleService {
 
@@ -28,5 +32,23 @@ public class ArticleService {
 
     public Iterable<Article> getAll() {
         return articleRepository.findAll();
+    }
+
+    public List<Article> getFeatured() {
+        List<Article> allArticles = (List<Article>) articleRepository.findAll();
+
+        List<Article> randomArticles = new ArrayList<>();
+        Random random = new Random();
+
+        while (randomArticles.size() < 8 && allArticles.size() >= 8) {
+            int randomIndex = random.nextInt(allArticles.size());
+            Article randomArticle = allArticles.get(randomIndex);
+
+            if (!randomArticles.contains(randomArticle)) {
+                randomArticles.add(randomArticle);
+            }
+        }
+
+        return randomArticles;
     }
 }
