dee4a80b93d84518d85aa339ccb9f112375c2ce2
Lukas <lukas.marovic5@gmail.com>
2025-01-12 22:19:53 +0100

    Test to see which account I'm using

--- IzvorniKod/frontend/src/Components/UserProfile.jsx
+++ IzvorniKod/frontend/src/Components/UserProfile.jsx
@@ -28,7 +28,6 @@ function UserProfile() {
     };
 
     useEffect(() => {
-        // console.log(user);
         const fetchUserDetails = async () => {
             if (document.cookie) {
                 try {
